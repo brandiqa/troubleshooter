12cff1cbbc86c86aa5f84bd7e88e8dbdb4f882b3
Michael Wanyoike <micwanyoike@gmail.com>
2017-04-12 21:07:50 +0300

    enabled backend authentication

--- backend/src/services/index.js
+++ backend/src/services/index.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const users = require('./users/users.service.js');
+
 module.exports = function () {
   const app = this; // eslint-disable-line no-unused-vars
+  app.configure(users);
 };
