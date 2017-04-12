344467a135aa3d2fa55b2169bda418c0eb0c036c
Michael Wanyoike <micwanyoike@gmail.com>
2017-04-12 11:49:01 +0300

    Added navlinks to dashboard

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,29 +1,29 @@
 import React, { Component } from 'react';
 import DevTools from 'mobx-react-devtools';
+import { NavLink } from 'react-router-dom';
 import { Sidebar, Segment, Menu, Image, Icon } from 'semantic-ui-react';
 import logo from './logo-inverted.svg';
+import icon from './icon.svg';
 
 class Dashboard extends Component {
   render() {
     return (
       <div>
-        <DevTools/>
         <Sidebar.Pushable as={Segment} style={{height:"100vh"}}>
-          <Sidebar as={Menu} width='thin' vertical  visible>
-            <Image src={logo} />
-            <Menu.Item name='dashboard'>
-              <Icon name='home' />
-              Dashboard
-            </Menu.Item>
-            <Menu.Item name='users'>
-              <Icon name='users' />
-              Users
-            </Menu.Item>
+          <Sidebar as={Menu} width='thin' vertical visible>
+            <Segment basic centered style={{height:"50px"}}></Segment>
+            <NavLink className="item" activeClassName="active" exact to="/">
+              <Icon name='home' /> Dashboard
+            </NavLink>
+            <NavLink className="item" activeClassName="active" exact to="/users">
+              <Icon name='users' /> Users
+            </NavLink>
           </Sidebar>
           <Sidebar.Pusher>
             <Segment basic>
-              <h1>My Dashboard</h1>
+              <Image src={logo} size="small"/>
               <hr/>
+              <DevTools/>
               {this.props.children}
             </Segment>
           </Sidebar.Pusher>
